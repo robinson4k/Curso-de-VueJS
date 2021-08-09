8c63c9e3e62d82f1a40586c6ab7cf554182d6c02
Robinson Silva <robinson.silva@piauiconectado.com.br>
2021-08-09 17:23:11 -0400

    67. Implementando Atacar

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -16,7 +16,14 @@ new Vue({
             this.playerLife = this.monsterLife = 100
         },
         attack(especial) {
-
+            this.hurt('monsterLife', 5, 10, especial)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        hurt(prop, min, max, especial) {
+            const plus = especial ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+            return hurt
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
@@ -24,5 +31,8 @@ new Vue({
         },
     },
     watch: {
+        hasResult(value) {
+            if (value) this.running = false
+        }
     },
 })
